55e75ae3f2c1f42a51c8c05a48c03cb2bff46ecf
mkozhevnikov <mkozhevnikov@servicetitan.com>
2021-11-03 20:01:18 -0700

    init commit

---
+++ submerge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Some arguments are expected");
+        }
+        else
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"You passed: {arg}");
+            }
+        }
+    }
+}
